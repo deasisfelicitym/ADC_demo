c2e00c0dd212ae19dfa07383e11572b220b29ada
deasisfelicitym <deasisfelicitym@gmail.com>
2026-09-17 00:32:13 +0800

    potentiometer

--- ADC_demo.ino
+++ ADC_demo.ino
@@ -1,21 +1,11 @@
 /*====================================================
-                 ADC DEMO
+                 ADC DEMO 
 ======================================================
 Description:
-  This program uses the ADC (Analog-to-Digital Converter)
-  to read the brightness level detected by a photoresistor
-  (LDR). The LDR changes its resistance depending on the
-  amount of light it receives, allowing the system to
-  determine whether the surroundings are dark or bright.
-
-  The detected brightness is divided into three levels.
-  Each level corresponds to a different LED color:
-  Level 1 - Green
-  Level 2 - Yellow
-  Level 3 - Red
-
-  PWM (Pulse Width Modulation) is used to control the
-  LEDs based on the brightness level detected by the LDR.
+  This code uses ADC with a potentiometer to read its 
+  value, which is converted into LED brightness using 
+  PWM. The potentiometer value is also displayed in 
+  the Serial Monitor.
 
 Programmer:
   De Asis, Felicity M.
@@ -25,63 +15,32 @@ Programmer:
 Date:
   16 September 2026
 ----------------------------------------------------*/
-// GPIOS
+// GPIOs
 const uint8_t POT = 36;
+const uint8_t LED = 32;
 
-const uint8_t LED_G = 32;
-const uint8_t LED_Y = 33;
-const uint8_t LED_R = 25;
-
-// PWM
+// PWM config
 const int PWM_FREQ = 5000;
 const byte PWM_RES = 12;
 
-// variables
+// Variable
 int pot_val = 0;
 
 void setup() {
-
   // ADC pin
   pinMode(POT, INPUT);
 
-  // PWM setup
-  ledcAttach(LED_G, PWM_FREQ, PWM_RES);
-  ledcAttach(LED_Y, PWM_FREQ, PWM_RES);
-  ledcAttach(LED_R, PWM_FREQ, PWM_RES);
-
   Serial.begin(9600);
+
+  ledcAttach(LED, PWM_FREQ, PWM_RES);
 }
 
 void loop() {
-
   pot_val = analogRead(POT);
 
-  if (pot_val >= 0 && pot_val <= 1365) {
+  ledcWrite(LED, pot_val);
 
-    ledcWrite(LED_G, pot_val);
-    ledcWrite(LED_Y, 0);
-    ledcWrite(LED_R, 0);
-
-    Serial.println("Brightness: Level 1");
-
-  } 
-  else if (pot_val >= 1366 && pot_val <= 2730) {
-
-    ledcWrite(LED_G, 0);
-    ledcWrite(LED_Y, pot_val);
-    ledcWrite(LED_R, 0);
-
-    Serial.println("Brightness: Level 2");
-
-  } 
-  else if (pot_val >= 2731 && pot_val <= 4095) {
-
-    ledcWrite(LED_G, 0);
-    ledcWrite(LED_Y, 0);
-    ledcWrite(LED_R, pot_val);
-
-    Serial.println("Brightness: Level 3");
-  }
+  Serial.println(pot_val);
 
   delay(500);
 }
